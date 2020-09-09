d7fc47bc18b4deedd58b47fc831a98c404cbd5ef
dinesh2 <dineshchan72@gmail.com>
2020-09-09 13:35:34 +0545

    checkout right checkout

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route path="/checkout">
             <Checkout />
           </Route>
-          <Route path="/">
+          <Route path="/" exact>
             <Home />
           </Route>
         </Switch>
